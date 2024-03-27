62dae5bfb50b0a252ad2a3b8bb66029e9922a38a
CalebErnst01 <caleb.ernst2001@gmail.com>
2024-03-27 18:41:00 -0500

    Made non-functional sidemenu buttons
    Next push will have the redone divs & collapsing menus

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -342,5 +342,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    //toggles sidebar display
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+        
+        for (i = 0; i < coll.length; i++) {
+          coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+              content.style.display = "none";
+            } else {
+              content.style.display = "block";
+            }
+          });
+        }
+
 // Initial chart display
 showLineChart();
